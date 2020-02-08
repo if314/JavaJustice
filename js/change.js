5c474bd884887c5cdb83275cc7f30f1aabedaa5e
Iva Keranova <keranovai@gmail.com>
2020-02-08 14:13:46 -0500

    update
    update

---
+++ js/change.js
@@ -0,0 +1,7 @@
+	    function change() // no ';' here
+{
+    var elem = document.getElementById("myButton1");
+    if (elem.value=="Buy Now") elem.value = "CHECK OUT!";
+    else elem.value = "Buy Now";
+}
+   
