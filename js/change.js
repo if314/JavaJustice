81b4a1d79cda0d66f48dfbcb464dc6f2f3180f85
i <keranovai@gmail.com>
2020-02-11 12:46:52 -0500

    update
    final

--- js/change.js
+++ js/change.js
@@ -1,8 +1,16 @@
 jQuery (".fold")    .click (function () {
     if (jQuery (this).children (".fold_p").text() == "Add to Cart") {
             jQuery (this).children (".fold_p").text("CHECK OUT");
+
     }
     else {
          jQuery (this).children (".fold_p").text("Add to Cart");        
     }
 });
+
+
+
+
+
+
+
